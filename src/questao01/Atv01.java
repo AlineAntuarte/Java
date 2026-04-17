0ab23ff92e4d9e1c11e9124e8bc7fe02599cd1e4
AlineAntuarte <aline.ba@aluno.ifsc.edu.br>
2026-04-16 21:41:13 -0300

    .

--- src/questao01/Atv01.java
+++ src/questao01/Atv01.java
@@ -1,6 +1,6 @@
-package questao02;
+package questao01;
 
-public class App02 {
+public class Atv01 {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
     }
--- src/questao02/Atv02.java
+++ src/questao02/Atv02.java
@@ -1,6 +1,6 @@
-package questao01;
+package questao02;
 
-public class App01 {
+public class Atv02 {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
     }
